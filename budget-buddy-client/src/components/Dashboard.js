84224499e4f4b5f7c8a049a64f6b8f621ae6d926
Namima Islam <namimaislam308@gmail.com>
2024-12-31 15:08:36 -0500

    new

--- budget-buddy-client/src/components/Dashboard.js
+++ budget-buddy-client/src/components/Dashboard.js
@@ -36,6 +36,11 @@ function Dashboard() {
             setError("All fields are required.");
             return;
         }
+
+        if (parseFloat(expenseToValidate.amount) <= 0) {
+        setError("Amount must be greater than 0.");
+        return;
+        }
     
         const username = localStorage.getItem("username"); // Get the logged-in user's username
         const url = editingExpense
